c2c49a7578a3cfa729a7addbad86f1fd655194a9
Harii2K4 <harikishan2k4@gmail.com>
2026-09-03 16:04:37 +0530

    Add flapping of bird and game logic for start and restart

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const GAME_WIDTH = 1534;
 const GAME_HEIGHT = 830;
+const HORZ_VEL = 2;
 
 let Game = {
   isRunning: false,
@@ -9,13 +10,26 @@ let Game = {
 let Bird = {
   height: 50,
   width: 50,
-  img: "flappybird0.png",
+  frameCount: 0,
+  imgIdx: 0,
+  images: [],
   x: 50,
   y: Math.floor(GAME_HEIGHT / 2),
+
+  update() {
+    this.frameCount++;
+    if (this.frameCount % 8 === 0) {
+      this.imgIdx = (this.imgIdx + 1) % 4;
+    }
+    this.x += HORZ_VEL;
+  },
+
+  getImg() {
+    return this.images[this.imgIdx];
+  },
 };
 let ctx;
 let bg_img;
-let bird_img;
 
 window.onload = function () {
   const canvas = document.querySelector("canvas");
@@ -28,23 +42,24 @@ window.onload = function () {
   bg_img.onload = function () {
     ctx.drawImage(bg_img, 0, 0, GAME_WIDTH, GAME_HEIGHT);
   };
-
-  bird_img = new Image();
-  bird_img.src = Bird.img;
-  bird_img.onload = function () {
-    ctx.fillRect(Bird.x, Bird.y, Bird.width, Bird.height);
-    ctx.drawImage(bird_img, Bird.x, Bird.y, Bird.width, Bird.height);
-  };
+  let bird_img;
+  for (let i = 0; i < 4; i++) {
+    bird_img = new Image();
+    bird_img.src = `flappybird${i}.png`;
+    bird_img.onload = function () {};
+    Bird.images[i] = bird_img;
+  }
 };
 function render() {
   ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
   ctx.drawImage(bg_img, 0, 0, GAME_WIDTH, GAME_HEIGHT);
   ctx.fillRect(Bird.x, Bird.y, Bird.width, Bird.height);
-  ctx.drawImage(bird_img, Bird.x, Bird.y, Bird.width, Bird.height);
+  ctx.drawImage(Bird.getImg(), Bird.x, Bird.y, Bird.width, Bird.height);
 }
 function reset() {
   Bird.x = 50;
   Bird.y = Math.floor(GAME_HEIGHT / 2);
+  Bird.imgIdx = 0;
 }
 window.main = function () {
   Game.mainStopId = window.requestAnimationFrame(main);
@@ -56,7 +71,7 @@ window.main = function () {
     window.cancelAnimationFrame(Game.mainStopId);
     return;
   } else {
-    Bird.x += 2;
+    Bird.update();
   }
 
   //render
